537bc02914bdd649af1d0206079f8e710d33cedf
AleksandarPopovic97 <acapopce97@gmail.com>
2021-04-08 18:37:56 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout/Layout';
+import LandingPage from './containers/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <LandingPage></LandingPage>
+      </Layout>
     </div>
   );
 }
 
 export default App;
+
+// Aleksandar Popovic
+// • 2.1, 2.5.1, 2.5.4, 2.6.5.3, 2.6.6, 2.6.7
+// Dejan Males
+// • 2.2, 2.4, 2.5.2, 2.6.5.1, 2.6.4, 2.6.8
+// Ilija Marinovic
+// • 2.3, 2.5.3, 2.5.4, 2.6.1, 2.6.2, 2.6.3, 2.6.5.2, 2.6.9
