6e27bcd614da8056adb763490c39d7fa14f800b2
malesdejan54 <58633339+malesdejan54@users.noreply.github.com>
2021-06-28 20:12:34 +0200

    Admin crew adding and crew update.

--- src/components/CrewAdding/CrewAdding.js
+++ src/components/CrewAdding/CrewAdding.js
@@ -1,11 +1,71 @@
-import react from 'react';
+import axios from 'axios';
+import react, { useEffect, useState } from 'react';
+import CrewPicker from '../CrewAdding/CrewPicker';
 import DashboardLayout from '../DashboardLayout/DashboardLayout';
+import classes from './CrewAdding.module.css'
 
 const CrewAdding = (props) =>
 {
+    const [users, setUsers] = useState([])
+    const [crews, setCrews] = useState([])
+
+
+    useEffect(() => {
+        axios.get('http://localhost:60259/api/Users')
+            .then(function (response) {
+                // handle success
+                console.log(response.data);
+                setUsers(response.data);
+            })
+            .catch(function (error) {
+                // handle error
+            })
+            .then(function () {
+                // always executed
+            });
+
+            axios.get('http://localhost:60259/api/Crews')
+            .then(function (response) {
+                // handle success
+                console.log(response.data);
+                setCrews(response.data);
+            })
+            .catch(function (error) {
+                // handle error
+            })
+            .then(function () {
+                // always executed
+            });
+    },[])
+
+    const changeCrewHandler = (user, selectedCrew) => {
+        user.crew = selectedCrew;
+        axios.put('http://localhost:60259/api/Users/' + user.id, user)
+            .then(function (response) {
+                // handle success3
+                setUpdated(true);
+                console.log(response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+    }
+
     return(
         <DashboardLayout title="Adding users at crew" {...props}>
-            <h1>Adding members with role crew at existing crew</h1>
+            <div className={classes.CrewAdding}>
+
+            {users.map((user) => {
+                if(user.role === 'Crew member'){
+                    return <CrewPicker key={user.id} user={user} changeCrewHandler={changeCrewHandler} crews={crews}></CrewPicker>
+                }
+            })}
+            
+            </div>
         </DashboardLayout>
     )
 }
