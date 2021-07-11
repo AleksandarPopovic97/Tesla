639149df1088bb3080413bc681eb82c3c6a465df
ilijam24 <ilija.marinovic1997@gmail.com>
2021-07-11 21:34:46 +0200

    update SwitchingPlanPreview

--- src/containers/SwitchingPlan/SwitchingPlanPreview/SwitchingPlanPreview.js
+++ src/containers/SwitchingPlan/SwitchingPlanPreview/SwitchingPlanPreview.js
@@ -10,13 +10,12 @@ const SwitchingPlanPreview = (props) => {
         console.log( "hej " + props)
     } ,[])
 
-   
-
+    
     return(
-        <div >
-
+        <div>
             <h1>Switch Plan: {props.switchingPlan.id}</h1>
             <h3>Basic info</h3>
+           
             <div>
                 <p>Type: <strong>{props.switchingPlan.type}</strong></p>
                 <p>Status: <strong>{props.switchingPlan.status}</strong></p>
@@ -24,10 +23,28 @@ const SwitchingPlanPreview = (props) => {
                 <p>Note: <strong>{props.switchingPlan.notes}</strong></p>
                 <p>Purpose: <strong>{props.switchingPlan.purpose}</strong></p>
             </div>
-           
-              
+            
+                <h3>Devices</h3>
+                {
+                    props.switchingPlan.equipment ? props.switchingPlan.equipment.map(device => {
+                        return 
+                            <div>
+                                <p>Type: {device.type}</p>
+                                <p>Name: {device.name}</p>
+                                <p>Address: {device.address}</p>
+                                <p>Coordinates: {device.coordinates}</p>
+                                <hr></hr>
+                            </div>
+                        })  : null
+                } 
 
         </div>
+            
+                
+
+           
+
+        
     )
 }
 
